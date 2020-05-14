c236e275f7a55507f8f19fcc077404f3b26acf42
y715459784 <715459784@qq.com>
2020-05-14 12:53:23 +0800

    登录表单校验

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './permission'
 import store from './store'
 import Vant from 'vant'
-import 'vant/lib/index.css'
+import 'vant/lib/index.less'
 import '@/styles/index.less' // 引入全局的自定义样式  因为要覆盖vant的样式
 import 'amfe-flexible' // 引入自动适配插件
 
